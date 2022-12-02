4598af3d69ca4254d066cc1a4cd92a3958ddef15
michalskimateusz <michalskimateusz@outlook.com>
2022-12-02 14:47:57 +0100

    toast notification

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,13 +1,24 @@
 import Image from "next/image";
 import { urlFor } from "../sanity";
 import { ShoppingCartIcon } from "@heroicons/react/24/outline";
+import { useDispatch } from "react-redux";
+import { addToBasket } from "../redux/basketSlice";
+import toast from "react-hot-toast";
 
 interface IProps {
   product: Product;
 }
 
 const Product = ({ product }: IProps) => {
-  const addItemToBasket = () => {};
+  const dispatch = useDispatch();
+
+  const addItemToBasket = () => {
+    dispatch(addToBasket(product));
+
+    toast.success(`${product.title} added to basket`, {
+      position: "bottom-center",
+    });
+  };
 
   return (
     <div className="flex h-fit w-[320px] select-none flex-col space-y-3 rounded-xl bg-[#35383C] p-8 md:h-[500px] md:w-[400px] md:p-10">
